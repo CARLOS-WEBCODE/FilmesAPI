d18e0b64b63fbe7d9a1baed2b5de28e14e9f575c
Antonio Barbosa <acrb1108@gmail.com>
2024-10-14 23:41:37 -0300

    Criando Controller e Models.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,17 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    public void AdicionarFilme(Filme filme)
+    {
+        filmes.Add(filme);
+    }
+
+}
