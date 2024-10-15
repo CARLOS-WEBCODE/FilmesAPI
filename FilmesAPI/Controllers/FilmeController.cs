b64927b455bfa32f4634b2127b86cd504596c5f3
Antonio Barbosa <acrb1108@gmail.com>
2024-10-15 00:50:17 -0300

    Alterando a launchsettings

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -10,9 +10,12 @@ public class FilmeController : ControllerBase
     private static List<Filme> filmes = new List<Filme>();
 
     [HttpPost]
-    public void AdicionarFilme(Filme filme)
+    public void AdicionarFilme([FromBody] Filme filme)
     {
         filmes.Add(filme);
+
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
     }
 
 }
