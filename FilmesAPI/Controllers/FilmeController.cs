741e5215331c5b9f2741c3579c1f79a695be0bd1
Antonio Barbosa <acrb1108@gmail.com>
2024-10-14 23:54:41 -0300

    Inserindo o httppost

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -9,6 +9,7 @@ public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
 
+    [HttpPost]
     public void AdicionarFilme(Filme filme)
     {
         filmes.Add(filme);
